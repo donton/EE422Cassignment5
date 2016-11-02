c5b3d5012773fffed0fac8c489bbd15fcf559633
Don Ton <donton@utexas.edu>
2016-11-02 12:43:20 -0500

    Added header

--- src/assignment5/Main.java
+++ src/assignment5/Main.java
@@ -1,3 +1,16 @@
+/* CRITTERS GUI <MyClass.java>
+ * EE422C Project 4b submission by
+ * Domino Weir
+ * drw2583
+ * 16445
+ * Don Ton
+ * dt22776
+ * 16445
+ * Slip days used: <0>
+ * Fall 2016
+ * Fall 2016
+ */
+
 package assignment5;
 
 public class Main {
